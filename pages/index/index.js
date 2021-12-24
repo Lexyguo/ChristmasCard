8dff6753a538c953bc9c466c4cb6c8a33932f32b
Lexyguo <1241963177@qq.com>
2021-12-24 11:35:48 +0800

    [Add]:shinning animate

--- pages/index/index.js
+++ pages/index/index.js
@@ -24,7 +24,6 @@ Page({
     this.audioCtx.src = 'http://antiserver.kuwo.cn/anti.s?rid=MUSIC_56209792&response=res&format=mp3|aac&type=convert_url&br=128kmp3&agent=iPhone&callback=getlink&jpcallback=getlink.mp3'
     this.audioCtx.loop = true
     this.audioCtx.seek = 50
-    this.audioCtx.stop()
     this.audioCtx.onPlay(() => {
       console.log('开始播放')
     })
